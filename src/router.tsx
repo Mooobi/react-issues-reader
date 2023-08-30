8f32e29a362f1585da22b493b6d4a1fa5517f4ac
Mooobi <antod123@naver.com>
2023-08-30 23:28:29 +0900

    Feat: 에러 화면 구현

--- src/router.tsx
+++ src/router.tsx
@@ -2,16 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Detail from './pages/detail/Detail';
 import { PATH_DETAIL, PATH_MAIN } from './common/constants/constants';
+import NotFound from './pages/notFound/notFound';
 
 const router = createBrowserRouter([
-  {
-    path: PATH_MAIN,
-    element: <Main />,
-  },
-  {
-    path: PATH_DETAIL,
-    element: <Detail />,
-  },
+  { path: PATH_MAIN, element: <Main /> },
+  { path: PATH_DETAIL, element: <Detail /> },
+  { path: '*', element: <NotFound /> },
 ]);
 
 export default router;
