0524cd1b785ab09610e4887a312e899db04eaef3
Mooobi <antod123@naver.com>
2023-08-30 23:45:59 +0900

    Fix: import 파일 경로 수정

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Detail from './pages/detail/Detail';
 import { PATH_DETAIL, PATH_MAIN } from './common/constants/constants';
-import NotFound from './pages/notFound/notFound';
+import NotFound from './pages/notFound/NotFound';
 
 const router = createBrowserRouter([
   { path: PATH_MAIN, element: <Main /> },
