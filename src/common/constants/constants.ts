9739e991e885f23eb65b7fc8be4950a13fea48da
Mooobi <antod123@naver.com>
2023-08-30 21:27:35 +0900

    Fix: BASE_URL 수정

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -3,7 +3,7 @@ export const PATH_DETAIL = '/detail';
 
 export const ORGANIZATION_NAME = 'facebook';
 export const REPOSITORY_NAME = 'react';
-export const BASE_URL = `https://api.github.com/repos/${ORGANIZATION_NAME}/${REPOSITORY_NAME}/issues`;
+export const BASE_URL = `https://api.github.com`;
 
 export const AUTHOR = '작성자';
 export const CREATED_AT = '작성일';
