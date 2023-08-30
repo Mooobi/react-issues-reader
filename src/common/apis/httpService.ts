764900e13b6eeb6c285d5d925a682c0a02736e8c
Mooobi <antod123@naver.com>
2023-08-30 18:21:30 +0900

    Fix: 환경변수명 변경

--- src/common/apis/httpService.ts
+++ src/common/apis/httpService.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import { BASE_URL } from '../constants/URL';
 
+const { VITE_GH_TOKEN } = import.meta.env;
+
 export const httpService = axios.create({
   baseURL: BASE_URL,
   timeout: 1000,
 });
 
 httpService.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${process.env.GH_TOKEN}`;
+  if (VITE_GH_TOKEN) {
+    config.headers.Authorization = `Bearer ${VITE_GH_TOKEN}`;
+  }
 
   return config;
 });
