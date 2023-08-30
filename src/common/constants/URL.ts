522e99ac242fb5d482486eb81d3606c70b203040
Mooobi <antod123@naver.com>
2023-08-30 16:42:53 +0900

    Feat: header 구현

--- src/common/constants/URL.ts
+++ src/common/constants/URL.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = 'https://api.github.com/repos/facebook/react/issues';
+export const ORGANIZATION_NAME = 'facebook';
+
+export const REPOSITORY_NAME = 'react';
+
+export const BASE_URL = `https://api.github.com/repos/${ORGANIZATION_NAME}/${REPOSITORY_NAME}/issues`;
