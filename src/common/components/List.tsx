3bb5e89147b3f8871a903a75ae55504537141cb7
Mooobi <antod123@naver.com>
2023-08-31 20:48:53 +0900

    Fix: useObserver 커스텀훅 적용

--- src/common/components/List.tsx
+++ src/common/components/List.tsx
@@ -1,28 +1,37 @@
 import { styled } from 'styled-components';
 import useGetList from '../hooks/useGetList';
 import Card from './Card';
-import { useInView } from 'react-intersection-observer';
 import AdCard from './AdCard';
 import { CONTENT_COUNT } from '../constants/constants';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Loading from './Loading';
+import useObserver from '../hooks/useObserver';
 
 export default function List() {
   const [isLoading, setIsLoading] = useState(false);
-  const [ref, inView] = useInView();
-  const { list } = useGetList(inView, isLoading, setIsLoading);
+  const { entry, setTarget } = useObserver();
+  const { list, fetchData } = useGetList(isLoading, setIsLoading);
 
-  if (!list) return <Wrapper>Loading...</Wrapper>;
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (entry && entry.isIntersecting) {
+      fetchData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [entry]);
 
   return (
     <Wrapper>
       {list?.map((card, index) => (
         <div key={card.id}>
-          <Card card={card} />
+          <Card card={card} ref={list.length - 1 === index ? setTarget : null} />
           {!((index + 1) % CONTENT_COUNT) && <AdCard />}
         </div>
       ))}
-      <div ref={ref} />
       {isLoading && <Loading />}
     </Wrapper>
   );
